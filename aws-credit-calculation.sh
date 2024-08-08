4e848474933a757aad33242d6c04836807f925ed
nkuma1 <148466125+nkuma1@users.noreply.github.com>
2024-08-08 15:51:17 +0530

    Update aws-credit-calculation.sh

--- aws-credit-calculation.sh
+++ aws-credit-calculation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Custom list of AWS regions
-aws_regions=("us-east-1" "us-east-2" "us-west-1" "us-west-2" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "ap-south-1" "af-south-1" "ap-east-1" "ap-south-2" "ap-southeast-3" "ap-southeast-4" "ap-northeast-3" "ap-northeast-2" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ca-central-1" "eu-north-1" "sa-east-1")
+aws_regions=("us-east-1" "us-east-2" "us-west-1" "us-west-2" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "ap-south-1" "ap-northeast-3" "ap-northeast-2" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ca-central-1" "eu-north-1" "sa-east-1")
 
 # Services to count
 services_to_count=(
